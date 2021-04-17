7413bf1a0b87f412d7bc5f259f2483dfd34e36c4
Matt Moore <Matt.M.Moore13@outlook.com>
2021-04-17 15:55:23 -0400

    refactored the code for hooks

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { useState } from 'react'
+// import { withRouter } from 'react-router-dom'
 
 import { signIn } from '../../api/auth'
 import messages from '../AutoDismissAlert/messages'
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Layout from '../shared/layout'
 
-class SignIn extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      email: '',
-      password: ''
-    }
-  }
-
-  handleChange = event => this.setState({
-    [event.target.name]: event.target.value
+const SignIn = (props) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
   })
 
-  onSignIn = event => {
+  function setForm (key, value) {
+    setFormData({ ...formData, [key]: value })
+  }
+
+  // handleChange = event => this.setState({
+  //   [event.target.name]: event.target.value
+  // })
+
+  const { email, password } = formData
+
+  const onSignIn = event => {
     event.preventDefault()
+    const { email, password } = formData
+    const { msgAlert, history, setUser } = props
 
-    const { msgAlert, history, setUser } = this.props
-
-    signIn(this.state)
+    signIn({ email, password })
       .then(res => setUser(res.data.user))
       .then(() => msgAlert({
         heading: 'Sign In Success',
@@ -35,7 +38,10 @@ class SignIn extends Component {
       }))
       .then(() => history.push('/'))
       .catch(error => {
-        this.setState({ email: '', password: '' })
+        setFormData({
+          email: '',
+          password: ''
+        })
         msgAlert({
           heading: 'Sign In Failed with error: ' + error.message,
           message: messages.signInFailure,
@@ -44,14 +50,12 @@ class SignIn extends Component {
       })
   }
 
-  render () {
-    const { email, password } = this.state
-
-    return (
+  return (
+    <Layout>
       <div className="row">
         <div className="col-sm-10 col-md-8 mx-auto mt-5">
           <h3>Sign In</h3>
-          <Form onSubmit={this.onSignIn}>
+          <Form onSubmit={ onSignIn }>
             <Form.Group controlId="email">
               <Form.Label>Email address</Form.Label>
               <Form.Control
@@ -60,7 +64,7 @@ class SignIn extends Component {
                 name="email"
                 value={email}
                 placeholder="Enter email"
-                onChange={this.handleChange}
+                onChange={event => setForm('email', event.target.value)}
               />
             </Form.Group>
             <Form.Group controlId="password">
@@ -71,7 +75,7 @@ class SignIn extends Component {
                 value={password}
                 type="password"
                 placeholder="Password"
-                onChange={this.handleChange}
+                onChange={event => setForm('password', event.target.value)}
               />
             </Form.Group>
             <Button
@@ -83,8 +87,8 @@ class SignIn extends Component {
           </Form>
         </div>
       </div>
-    )
-  }
+    </Layout>
+  )
 }
 
-export default withRouter(SignIn)
+export default SignIn
