0e2ffb9ab0b3467a084b3faedc7b2ad7bc849a9a
Matt Moore <Matt.M.Moore13@outlook.com>
2021-04-17 15:46:25 -0400

    Refactored the code to use Hooks; added a home route

--- src/App.js
+++ src/App.js
@@ -4,39 +4,29 @@ import { v4 as uuid } from 'uuid'
 
 import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
 import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
-// import Header from './components/Header/Header'
+// import Header from './components/shared/Header'
 import SignUp from './components/SignUp/SignUp'
 import SignIn from './components/SignIn/SignIn'
 import SignOut from './components/SignOut/SignOut'
 import ChangePassword from './components/ChangePassword/ChangePassword'
-import Layout from './components/shared/layout.js'
+// import Home from './components/shared/Home'
 
-const App = () => {
-  // constructor (props) {
-  //   super(props)
-  //   this.state = {
-  //     user: null,
-  //     msgAlerts: []
-  //   }
-  // }
+const App = props => {
   const [user, setUser, clearUser] = useState(null)
-  const [msgAlerts, deleteAlert, msgAlert] = useState([])
-  
-  // setUser = user => this.setState({ user })
-  // 
-  // clearUser = () => this.setState({ user: null })
+  const [msgAlerts, setMsgAlerts] = useState([])
 
-  deleteAlert = (id) => {
-    this.setState((state) => {
-      return { msgAlerts: state.msgAlerts.filter(msg => msg.id !== id) }
-    })
+  const deleteAlert = (id) => {
+    setMsgAlerts([
+      ...msgAlerts.filter(msg => msg.id !== id)
+    ])
   }
 
-  msgAlert = ({ heading, message, variant }) => {
+  const msgAlert = ({ heading, message, variant }) => {
     const id = uuid()
-    this.setState((state) => {
-      return { msgAlerts: [...state.msgAlerts, { heading, message, variant, id }] }
-    })
+    setMsgAlerts([
+      ...msgAlerts,
+      { heading, message, variant, id }
+    ])
   }
 
   // render (props) {
@@ -44,32 +34,34 @@ const App = () => {
 
   return (
     <Fragment>
-      {msgAlerts.map(msgAlert => (
+      {msgAlerts.map((msgAlert) => (
         <AutoDismissAlert
           key={msgAlert.id}
           heading={msgAlert.heading}
           variant={msgAlert.variant}
           message={msgAlert.message}
           id={msgAlert.id}
-          deleteAlert={this.deleteAlert}
+          deleteAlert={deleteAlert}
         />
       ))}
-      <Layout />
+
+      <div className="App">
+        {props.location.state ? props.location.state.msg : null}
+      </div>
       <main className="container">
-        <Fragment id='userAuth'>
-          <Route path='/sign-up' render={() => (
-            <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword msgAlert={this.msgAlert} user={user} />
-          )} />
-        </Fragment>
+        {/* <Route exact path='/' component={Home} /> */}
+        <Route path='/sign-up' render={() => (
+          <SignUp msgAlert={msgAlert} setUser={setUser} />
+        )} />
+        <Route path='/sign-in' render={() => (
+          <SignIn msgAlert={msgAlert} setUser={setUser} />
+        )} />
+        <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+          <SignOut msgAlert={msgAlert} clearUser={() => clearUser({ user: null })} user={user} />
+        )} />
+        <AuthenticatedRoute user={user} path='/change-password' render={() => (
+          <ChangePassword msgAlert={msgAlert} user={user} />
+        )} />
       </main>
     </Fragment>
   )
